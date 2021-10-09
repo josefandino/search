ba8ae8ab35f42bb6409a53acfafd8e6939d0dbf8
dev_josefandino <piolinfan@gmail.com>
2021-10-09 12:35:33 -0500

    fetch, axios

--- src/views/search/search.js
+++ src/views/search/search.js
@@ -1,18 +1,46 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchBox from './components/searchBox/search-box'
 import './search.css';
 
-import data from '../../data/users.json';
+// import data from '../../data/users.json';
+
 import SearchResult from './components/searchResults/searchResults';
 import SearchResultsItems from './components/searchResults/searchResultsItems';
+import axios from 'axios';
 
 export default function Search() {
-
+  
   const [isAtTop, setIsAtTop] = useState(false);
-
+  
   // const [userData, setUsersData] = useState(data);
   const [results, setResults] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        // const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      // const data = await response.json();
+        // Promesas
+        // .then(response => response.json())
+        // .then(data => {
+          // setData(data);
+          // console.log(data);
+      // AXIOS 
+      const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+      console.log(data)
+      setData(data);
+        // console.log(data);
+      // }
+    // );
+      } catch (error) {
+        console.log(error)
+      }
+    };
+
+    getUsers().catch(null);
+  }, []);
 
   const handleCloseSearch = () => {
     setIsAtTop(false);
